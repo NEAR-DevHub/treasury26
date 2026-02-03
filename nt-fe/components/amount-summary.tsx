6a7b03cf3816def8737c5f9131ab45de865b6149
akorchyn <artur.yurii.korchynskyi@gmail.com>
2026-02-03 21:49:24 +0200

    css: style fixes

--- nt-fe/components/amount-summary.tsx
+++ nt-fe/components/amount-summary.tsx
@@ -17,14 +17,17 @@ export function AmountSummary({
     return (
         <InputBlock title="" invalid={false}>
             <div className="flex flex-col gap-2 p-2 text-xs text-muted-foreground text-center justify-center items-center">
-                <p>{title}</p>
+                <p className="font-medium">{title}</p>
                 <img
                     src={token.icon}
                     alt={token.symbol}
                     className="size-10 shrink-0 rounded-full"
                 />
                 <p className="text-xl font-semibold text-foreground">
-                    {total} {token.symbol}
+                    {total}{" "}
+                    <span className="text-muted-foreground font-medium text-sm">
+                        {token.symbol}
+                    </span>
                 </p>
                 <div>{children}</div>
             </div>
