934e4eed45294d5c804a6683eab0b259cf0ead10
akorchyn <artur.yurii.korchynskyi@gmail.com>
2026-02-05 17:29:51 +0200

    feat: support center modal

--- nt-fe/constants/config.ts
+++ nt-fe/constants/config.ts
@@ -8,4 +8,6 @@ export const NEAR_TREASURY_CONFIG = {
 export const APP_DOCS_URL = "https://docs.neartreasury.com/";
 export const APP_DEMO_URL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 export const APP_LEARN_URL = "https://docs.neartreasury.com";
+export const APP_TWITTER_URL = "https://x.com/trezu";
+export const APP_SUPPORT_URL = "mailto:support@neartreasury.com";
 export const LOCKUP_NO_WHITELIST_ACCOUNT_ID = "lockup-no-whitelist.near";
