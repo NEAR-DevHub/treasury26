9402272ca3e15b94b0cc46ead61e1937df411059
ktarun1419 <tarunkaushik355@gmail.com>
2026-06-04 17:39:00 +0530

    fixed merge conflict

--- nt-fe/features/proposals/components/expanded-view/swap-expanded.tsx
+++ nt-fe/features/proposals/components/expanded-view/swap-expanded.tsx
@@ -63,16 +63,20 @@ function IntentsSwapExpanded({ data, isExecuted = false }: SwapExpandedProps) {
     const shouldLoadQuoteUsd =
         isExecuted &&
         !!data.depositAddress &&
-        !(data.quoteAmountInUsd && data.quoteAmountOutUsd);
+        (data.amountInUsd == null || data.amountOutUsd == null);
     const { data: quoteByDepositAddress } = useQuoteByDepositAddress(
         data.depositAddress || null,
         undefined,
         shouldLoadQuoteUsd,
     );
     const sourceAmountUsdRaw =
-        data.quoteAmountInUsd ?? quoteByDepositAddress?.amountInUsd;
+        data.amountInUsd != null
+            ? String(data.amountInUsd)
+            : quoteByDepositAddress?.amountInUsd;
     const destinationAmountUsdRaw =
-        data.quoteAmountOutUsd ?? quoteByDepositAddress?.amountOutUsd;
+        data.amountOutUsd != null
+            ? String(data.amountOutUsd)
+            : quoteByDepositAddress?.amountOutUsd;
     const sourceAmountUsdOverride =
         sourceAmountUsdRaw && !Number.isNaN(Number(sourceAmountUsdRaw))
             ? formatCurrency(Number(sourceAmountUsdRaw))
