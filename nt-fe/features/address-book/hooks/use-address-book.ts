e73f3b92d0eb8946b75ade2317d590a53af52a61
ktarun1419 <tarunkaushik355@gmail.com>
2026-06-11 12:53:22 +0530

    formatting issue

--- nt-fe/features/address-book/hooks/use-address-book.ts
+++ nt-fe/features/address-book/hooks/use-address-book.ts
@@ -12,9 +12,9 @@ export function useAddressBook() {
         queryKey: ["address-book", treasuryId, accountId],
         queryFn: () => getAddressBook(treasuryId!),
         enabled,
-        staleTime: 1000* 10, //10 seconds
+        staleTime: 1000 * 10, //10 seconds
         refetchInterval: 1000 * 60, //1 minute
         refetchIntervalInBackground: true,
-        refetchOnWindowFocus: true
+        refetchOnWindowFocus: true,
     });
 }
